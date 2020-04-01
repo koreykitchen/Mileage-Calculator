632dd3c933a3657443cc396967e48e47310410f7
Korey Kitchen <koreykitchen@gmail.com>
2020-04-01 17:12:36 -0400

    Updates

---
+++ precache-manifest.b97e4b2359ce64580aa9a64ab5e12219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe249dbcd754b00e4f45cc0fb58b67cd",
+    "url": "/Mileage-Calculator/index.html"
+  },
+  {
+    "revision": "d1282348f6f8c607a32e",
+    "url": "/Mileage-Calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "024db6c8a32e588621e5",
+    "url": "/Mileage-Calculator/static/js/2.32838cfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mileage-Calculator/static/js/2.32838cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1282348f6f8c607a32e",
+    "url": "/Mileage-Calculator/static/js/main.74c5c795.chunk.js"
+  },
+  {
+    "revision": "382d964305a3b05d4bdf",
+    "url": "/Mileage-Calculator/static/js/runtime-main.1eaaa597.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mileage-Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
